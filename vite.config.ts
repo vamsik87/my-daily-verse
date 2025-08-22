1a8debb2d994d962b6df3867528603d6b9d76f7d
Vamsi Kapa <vamsikapa@Vamsis-MacBook-Pro.local>
2025-08-23 07:11:37 +1000

    asset/asset fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig, loadEnv } from 'vite';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: process.env.NODE_ENV === 'production' ? './' : '/',
+      base: process.env.NODE_ENV === 'production' ? '/my-daily-verse/' : '/',
       resolve: {
         alias: {
           '@': path.resolve(__dirname, '.'),
